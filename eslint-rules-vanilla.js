12e27337ac1c8152c23ea8171a2c9e5fb7ea8460
Katarina Polc <katarina.polc@ceneje.si>
2023-05-07 09:44:24 +0200

    Indent case in switch statement.

--- eslint-rules-vanilla.js
+++ eslint-rules-vanilla.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   rules: {
     semi: ['error', 'always'],
-    indent: ['error', 4],
+    indent: [ 'error', 4, { SwitchCase: 1 } ],
     quotes: ['error', 'single'],
     'object-property-newline': ['error', {allowAllPropertiesOnSameLine: false}],
     'object-curly-spacing': ['error', 'always'],
