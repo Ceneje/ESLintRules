e709fac83b6477a5884b45d72577c6ff1df64421
Katarina Polc <katarina.polc@ceneje.si>
2023-05-10 09:29:40 +0200

    Configure ESLint to ignore jsx elements.

--- eslint-rules-vanilla.js
+++ eslint-rules-vanilla.js
@@ -6,7 +6,7 @@ module.exports = {
   ignorePatterns: ['**/*.{jsx,tsx}'],
   rules: {
     semi: ['error', 'always'],
-    indent: [ 'error', 4, { SwitchCase: 1 } ],
+    indent: [ 'error', 4, { SwitchCase: 1, ignoredNodes: ['JSXElement *', 'JSXElement'] } ],
     quotes: ['error', 'single'],
     'object-property-newline': ['error', {allowAllPropertiesOnSameLine: false}],
     'object-curly-spacing': ['error', 'always'],
