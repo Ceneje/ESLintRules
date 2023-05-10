fc83ff8e04f4eedf9d7e6438fbd35d8ba83f15d1
Katarina Polc <katarina.polc@ceneje.si>
2023-05-10 10:50:12 +0200

    Align multiline props by the first prop.

--- eslint-rules-react.js
+++ eslint-rules-react.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'react-app/jest',
-  ],
-  rules: {
-    'react/jsx-max-props-per-line': [1, {'maximum': 1}],
-  },
+    extends: [
+        'react-app',
+        'react-app/jest',
+    ],
+    rules:   {
+        'react/jsx-max-props-per-line': [ 2, { 'maximum': 1 } ],
+        'react/jsx-indent-props':       [ 2, 'first' ]
+    },
 }
