ce1a282023ae91e3e7e50e37f91d955fc2159c36
Katarina Polc <katarina.polc@ceneje.si>
2023-11-10 08:51:11 +0100

    Add new eslint rules for react.

--- eslint-rules-react.js
+++ eslint-rules-react.js
@@ -3,8 +3,31 @@ module.exports = {
         'react-app',
         'react-app/jest',
     ],
-    rules:   {
-        'react/jsx-max-props-per-line': [ 2, { 'maximum': 1 } ],
-        'react/jsx-indent-props':       [ 2, 'first' ]
+    rules: {
+        'react/jsx-max-props-per-line': [1, {'maximum': 1}],
+        'react/jsx-no-target-blank':          0,
+        'react/jsx-one-expression-per-line':  1,
+        'react/jsx-indent':                   [ 2, 4, { indentLogicalExpressions: true } ],
+        'react/jsx-curly-spacing':            [
+            1, {
+                when:     'never',
+                children: false,
+                spacing:  { objectLiterals: 'never' },
+            },
+        ],
+        'react/jsx-curly-newline':            1,
+        'react/jsx-closing-tag-location':     2,
+        'react/jsx-closing-bracket-location': 2,
+        'react/jsx-wrap-multilines':          [
+            2, {
+                declaration: 'ignore',
+                assignment:  'ignore',
+                return:      'parens-new-line',
+                arrow:       'ignore',
+                condition:   'ignore',
+                logical:     'ignore',
+                prop:        'ignore',
+            },
+        ],
     },
 }
